d216005ef8e281a7f8974c404ff49bafc31e72a8
maximusavs <ofxsite@gmail.com>
2019-05-01 11:57:49 +0400

    версия 12

--- build/generated/source/buildConfig/conversationsFreeCompat/debug/com/ru/astron/BuildConfig.java
+++ build/generated/source/buildConfig/conversationsFreeCompat/debug/com/ru/astron/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ru.astron";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "conversationsFreeCompat";
-  public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "1.0.4+fcd";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "1.0.6";
   public static final String FLAVOR_mode = "conversations";
   public static final String FLAVOR_distribution = "free";
   public static final String FLAVOR_emoji = "compat";
